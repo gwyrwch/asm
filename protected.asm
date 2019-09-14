dd0d2ea962eddc78124b5045a18b42961921799a
gwyrwch <gwyrwch@gmail.com>
2019-09-14 20:04:41 +0300

    semistable lab1

--- protected.asm
+++ protected.asm
@@ -88,11 +88,11 @@ main:
     ;out 92h,al
 
     lgdt [GDTR]  
-    lidt [IDTR]
+    ; lidt [IDTR]
 
     call set_PE 
 
-    ;jmp $
+    ; jmp $
     jmp fword 8:protected_mode_begining;
 
 
@@ -158,157 +158,42 @@ protected_mode_begining:
     mov  esi, hi_string  
     cld
 
-    int 1
- 
-    jmp $
-
-
-
-    ;jmp $
-;========================================
-; задачи
-;========================================
-
-
-; задача 1
-;task_1:
-;        mov        byte ptr ds:[edi],al ; вывести символ на экран
-;        inc        al                   ; увеличить код символа
-;        add        edi,2                ; увеличить адрес символа
-; переключиться на задачу 0
-;        db         0EAh
-;        dd         0
-;        dw         SEL_TSS0
-; сюда будет приходить управление, когда задача 0 начнет выполнять переход
-; на задачу 1 во всех случаях, кроме первого
-;        mov        ecx,02000000h        ; небольшая пауза, зависящая от скорости
-;        loop       $                    ; процессора
-;        jmp        task_1
-;========================================
-; Обработчики прерываний
-;========================================
-test_handler:
-	
-
-
-int9_handler:
-    push ax
-    push edi
-    xor  ax, ax
-
-    ; запрашиваем позиционный код клавиши
-    in   al, 060h
-
-    dec  al   ; Нажат ли <Esc> ? (его сканкод = 1)
-    jnz _continue_handling
-
-    add  edi, 0xA0 
-    mov  esi, esc
-    int 1
-
-    pop  edi
-    pop  ax
-    ;jmp  int_EOI
-    call pressed_esc
-
-_continue_handling:
-  
-    ; посылка подтверждения обрабоки в порт клавиатуры
-    ; (установка и сброс 7 бита порта 061h)
-   Ack:
-    in   al, 061h
-    or   al, 80
-    out  061h, al
-    xor  al, 80
-    out  061h, al
-
-clear_request:
-    pop  edi
-    pop  ax
-    jmp  int_EOI
-
-print_handler:
     pushad
 
-    loo:                     
+    lo1:                     
         lodsb
         test al, al
-        jz   .exit
+        jz   .ex1
         stosb
         mov  al, 7
         stosb
-        jmp  loo
+        jmp  lo1
 
-    .exit:
+    .ex1:
     popad
-    ;int 2
-    iret
-
-task_1:
-
-	lodsb
-	test al, al 	
-	stosb
-	
-	
-	iret
-
-exGP_handler:
-    pop  eax 
-    add  edi, 0xA0
-    mov  esi, gp
-    int  1
-    iret
-
-int8_handler:
-
-    add  edi, 0xA4
-
-    push esi
-    mov esi, hi_string
-    int 1
-	pop esi
-	
-
-    push edi
-    mov edi, 0xA8000
-    int 2
-    pop edi
-    
-    jmp  int_EOI     ; сбросим заявку на прерывание
 
 
+    add  edi, 0xA0 
+    mov  esi, hi_real  
 
-int_EOI:
-    ; сброс заявки в контроллере прерываний: посылка End-Of-Interrupt (EOI) ...
-    push ax
-    mov  al, 20h
-    out  020h, al   ; ... в ведущий (Master) контроллер ...
-    out  0a0h, al   ; ... и в ведомый (Slave) контроллер.
-    pop  ax
-    iret           ; возврат из прерывания
+    pushad
+    lo2:                     
+        lodsb
+        test al, al
+        jz   .ex2
+        stosb
+        mov  al, 7
+        stosb
+        jmp  lo2
 
+    .ex2:
+    popad
 
-
-stub:
-    pusha
-    ; interrupt handler for isr0-33
-	popa
-	iret
-
-ascii    db 0,'1234567890-+',0,0,'QWERTYUIOP[]',0,0,'ASDFGHJKL;',"'`",0,0,'ZXCVBNM,./',0,'*',0,' ',0, 0,0,0,0,0,0,0,0,0,0, 0,0, '789-456+1230.', 0,0
-esc db '** ESC **',0
-
-
-pressed_esc:
-    	add  edi, 0xA0 
-    	mov  esi, hi_real  
-
-    	int 1
-    	call disable_interrupts
-    	
-    	jmp fword 32:protected_mode_exiting
-
+    call disable_interrupts
+        
+    jmp fword 32:protected_mode_exiting
+ 
+    ; jmp $
 
 ;========================================
 ; Код 16бит
@@ -326,7 +211,6 @@ protected_mode_exiting:
 
 
 
-
 real_mode:
 
     mov ss,[save_ss]
@@ -337,8 +221,6 @@ real_mode:
     mov gs,[save_gs]
     
 
-    lidt [IDTR]
-
     in         al,70h              ; индексный порт CMOS
     and        al,07Fh             ; сброс бита 7 отменяет блокирование NMI
     out        70h,al
@@ -348,10 +230,9 @@ real_mode:
 
 
 hi_real db 'Hello from Real', 0
-task_0_string db 'Task0',0
 hi_string db 'Hello from Protected', 0
-string   db '  Switched to ProtectedMode. Press <Esc> to clear display', 0
-gp db '** GENERAL PROTECTION FAULT **',0
+
+
 ;========================================
 ;; Таблица дескрипторов
 ;========================================
@@ -374,7 +255,7 @@ gdt32:
     db 0x0    ; segment base, bits 24-31
 
     
-    ; data segment read&write
+    ; data32 segment read&write
     dw 0xFFFF
     dw 0x0
     db 0x1
@@ -387,7 +268,7 @@ gdt32:
     db 0x0
 
     
-    ; data segment read&write (video segment)
+    ; data32 segment read&write (video segment)
     dw 0xFFFF
     dw 0x8000
     db 0x0B
@@ -397,7 +278,7 @@ gdt32:
     ;GDXU
     db 0x0
 
-    ; code segment
+    ; code16 segment
     dw 0xFFFF
     dw 0x0
     db 0x1
@@ -405,21 +286,13 @@ gdt32:
     db 0x0
     db 0x0
 
-    ; data segment 
+    ; data16 segment 
     dw 0xFFFF
     dw 0x0
     db 0x1
     db 0x92
     db 0x0
     db 0x0
-; сегмент TSS задачи 0 (32-битный свободный TSS)
-    dw 067h
-    dw 0
-    db 0
-    db 10001001b
-    db 01000000b
-    db 0
-
 
 label GDT_SIZE at $-gdt32
 GDTR:
@@ -427,257 +300,5 @@ GDTR:
     dd gdt32+10000h
 
 
-IDT:
-;    dd 0x0, 0x0
-
-;0:
-    dw stub
-    dw 0x0008
-    db 0x0
-    db 10101110b
-    dw 0x0000
-
-;1:
-    ;dw stub
-    dw print_handler
-    dw 0x0008
-    db 0x0
-    db 10001110b
-    dw 0x0000
-
-;2:
-    dw task_1
-    dw 0x0008
-    db 0x0
-    db 10101110b
-    dw 0x0000
-
-;3:
-    dw stub
-    dw 0x0008
-    db 0x0
-    db 10101110b
-    dw 0x0000
-
-;4:
-    dw stub
-    dw 0x0008
-    db 0x0
-    db 10101110b
-    dw 0x0000
-
-;5:
-    dw stub
-    dw 0x0008
-    db 0x0
-    db 10101110b
-    dw 0x0000
-
-;6:
-    dw stub
-    dw 0x0008
-    db 0x0
-    db 10101110b
-    dw 0x0000
-
-;7:
-    dw stub
-    dw 0x0008
-    db 0x0
-    db 10101110b
-    dw 0x0000
-
-;8:
-    dw int8_handler
-    dw 0x0008
-    db 0x0
-    db 10001110b
-    dw 0x0000
-
-;9:
-    dw stub
-    dw 0x0008
-    db 0x0
-    db 10101110b
-    dw 0x0000
-
-;10:
-    dw stub
-    dw 0x0008
-    db 0x0
-    db 10101110b
-    dw 0x0000
-
-;11:
-    dw stub
-    dw 0x0008
-    db 0x0
-    db 10101110b
-    dw 0x0000
-
-;12:
-    dw stub
-    dw 0x0008
-    db 0x0
-    db 10101110b
-    dw 0x0000
-
-;13:
-    dw exGP_handler
-    dw 0x0008
-    db 0x0
-    db 10001110b
-    dw 0x0000
-
-;14:
-    dw stub
-    dw 0x0008
-    db 0x0
-    db 10101110b
-    dw 0x0000
-
-;15:
-    dw stub
-    dw 0x0008
-    db 0x0
-    db 10101110b
-    dw 0x0000
-
-;16:
-    dw stub
-    dw 0x0008
-    db 0x0
-    db 10101110b
-    dw 0x0000
-
-;17:
-    dw stub
-    dw 0x0008
-    db 0x0
-    db 10101110b
-    dw 0x0000
-
-;18:
-    dw stub
-    dw 0x0008
-    db 0x0
-    db 10101110b
-    dw 0x0000
-
-;19:
-    dw stub
-    dw 0x0008
-    db 0x0
-    db 10101110b
-    dw 0x0000
-
-;20:
-    dw stub
-    dw 0x0008
-    db 0x0
-    db 10101110b
-    dw 0x0000
-
-;21:
-    dw stub
-    dw 0x0008
-    db 0x0
-    db 10101110b
-    dw 0x0000
-
-;22:
-    dw stub
-    dw 0x0008
-    db 0x0
-    db 10101110b
-    dw 0x0000
-
-;23:
-    dw stub
-    dw 0x0008
-    db 0x0
-    db 10101110b
-    dw 0x0000
-
-;24:
-    dw stub
-    dw 0x0008
-    db 0x0
-    db 10101110b
-    dw 0x0000
-
-;25:
-    dw stub
-    dw 0x0008
-    db 0x0
-    db 10101110b
-    dw 0x0000
-
-;26:
-    dw stub
-    dw 0x0008
-    db 0x0
-    db 10101110b
-    dw 0x0000
-
-;27:
-    dw stub
-    dw 0x0008
-    db 0x0
-    db 10101110b
-    dw 0x0000
-
-;28:
-    dw stub
-    dw 0x0008
-    db 0x0
-    db 10101110b
-    dw 0x0000
-
-;29:
-    dw stub
-    dw 0x0008
-    db 0x0
-    db 10101110b
-    dw 0x0000
-
-;30:
-    dw stub
-    dw 0x0008
-    db 0x0
-    db 10001110b
-    dw 0x0000
-
-;31:
-    dw stub
-    dw 0x0008
-    db 0x0
-    db 10001110b
-    dw 0x0000
-
-;32:
-    dw int8_handler
-    dw 0x0008
-    db 0x0
-    db 10001110b
-    dw 0x0000
-
-;33:
-    dw int9_handler
-    dw 0x0008
-    db 0x0
-    db 10001110b
-    dw 0x0000
-
-IDTR:
-    dw $ - IDT - 1 ; size (16 bits), always one less of its true size
-    dd IDT + 0x1000 * 0x10
-.real_pointer:
-    dw 0x3FF
-    dd 0x0
 
 
-
-cursor   dd 0
-SEL_TSS0           equ   101000b
-SEL_TSS1           equ   110000b
